fe9aafcf2bdd3768c83c7e4318eabd81f02a1a0f
mbelan <mgbelan@gmail.com>
2016-01-05 16:11:23 -0500

    Most user functions working

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations'}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root 'items#index'
   delete 'items/delete/:id', to: 'items#destroy', as: :delete_item
   resources :items
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160105161620) do
+ActiveRecord::Schema.define(version: 20160105193453) do
 
   create_table "items", force: :cascade do |t|
     t.string   "title"
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20160105161620) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "user_name"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
